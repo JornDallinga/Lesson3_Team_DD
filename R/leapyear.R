60f46303e2ee7dd549fbcb7819aa51017a07d5ee
unknown <dalli001@D0117107.wurnet.nl>
2015-01-07 16:12:10 +0100

    Simplified the script by combining the year ranges

--- R/leapyear.R
+++ R/leapyear.R
@@ -1,21 +1,17 @@
 
-# function to calculate the leapyears
+# Function to calculate the leapyears
 leapyear <- function(year){
 
 # Return warning message when the input is not a number 
   if (!is.numeric(year)) {
     warning("input year is not numerical, please try again")
   
-# exclude the years that are below 1800
-  } else if (year < 1800) {
+# Exclude the years that are below 1800 or above 2400
+  } else if (year < 1800 | year > 2400) {
     warning("input year is out of range, please try again")
-
-# exclude the years that are above 2400
-  } else if (year > 2400) {
-    warning("input year is out of range, please try again")
-  } 
-# return TRUE if it is a leapyear, so divisable by 4 and 400, but not by 100
-    else {
+   
+# Return TRUE if it is a leapyear, so divisable by 4 and 400, but not by 100
+  } else {
     return (year %% 4 == 0) & (year %% 100 != 0) | (year %% 400 == 0)
   }
 
